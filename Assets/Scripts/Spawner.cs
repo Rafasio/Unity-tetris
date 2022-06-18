c56ba676ffb5e65f23d80edca8d86be7cf0fe13e
Rafasio <107765141+Rafasio@users.noreply.github.com>
2022-06-18 22:08:00 +0200

    commit

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject[] tetrominoes;
+
+    private void Start()
+    {
+        SpawnTetrominoe();
+    }
+
+    public void SpawnTetrominoe()
+    {
+        Instantiate(tetrominoes[Random.Range(0, tetrominoes.Length)],transform);
+    }
+
+}
