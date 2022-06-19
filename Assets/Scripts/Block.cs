62b617a7dfd83cc607cf0681e2189db358b008fb
Rafasio <107765141+Rafasio@users.noreply.github.com>
2022-06-19 14:22:36 +0200

    commit

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -9,10 +9,12 @@ public class Block : MonoBehaviour
     public float fallTime = 0.8f;
     public static int height = 80;
     public static int width = 10;
+    private GameObject spawner;
+    private static Transform[,] grid = new Transform[width,height];
     // Start is called before the first frame update
     void Start()
     {
-        
+        spawner = GameObject.FindGameObjectWithTag("Spawner");
     }
 
     // Update is called once per frame
@@ -21,7 +23,7 @@ public class Block : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             transform.position += new Vector3(-1, 0, 0);
-            if (!validMove())
+            if (!ValidMove())
             {
                 transform.position -= new Vector3(-1, 0, 0);
             }
@@ -30,7 +32,7 @@ public class Block : MonoBehaviour
         else if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             transform.position += new Vector3(1, 0, 0);
-            if (!validMove())
+            if (!ValidMove())
             {
                 transform.position -= new Vector3(1, 0, 0);
             }
@@ -38,10 +40,10 @@ public class Block : MonoBehaviour
         else if(Input.GetKeyDown(KeyCode.UpArrow))
         {
             transform.Rotate(rotationPoint, 90f);
-            Debug.Log("dziala");
-            if (!validMove())
+
+            if (!ValidMove())
             {
-                Debug.Log("nie dziala");
+
                 transform.Rotate(rotationPoint, -90f);
             }
                 
@@ -50,16 +52,86 @@ public class Block : MonoBehaviour
         if(Time.time - previousTime > (Input.GetKey(KeyCode.DownArrow) ? fallTime/10 :fallTime ) )
         {
             transform.position += new Vector3(0, -1, 0);
-            if (!validMove())
+            if (!ValidMove())
             {
                 transform.position -= new Vector3(0, -1, 0);
+                AddToGrid();
+                this.enabled = false;
+                spawner.GetComponent<Spawner>().SpawnTetrominoe();
+
             }
                 previousTime = Time.time;
         }
 
     }
 
-    bool validMove()
+    void AddToGrid()
+    {
+        foreach (Transform children in transform)
+        {
+
+            int roundedX = Mathf.RoundToInt(children.transform.position.x);
+            int roundedY = Mathf.RoundToInt(children.transform.position.y);
+
+            grid[roundedX, roundedY] = children;
+        }
+    }
+
+    void CheckForLines()
+    {
+        for (int i = height-1; i >= 0; i++)
+        {
+            if(HasLine(i))
+            {
+                DeleteLine(i);
+                RowDown(i);
+            }
+
+        }
+    }
+
+    bool HasLine(int i)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            if(grid[j,i]==null)
+            {
+                return false;
+            }
+
+        }
+        return true;
+    }
+
+    void DeleteLine(int i)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            Destroy(grid[j, i].gameObject);
+            grid[j, i] = null;
+
+        }
+
+    }
+
+    void RowDown(int i)
+    {
+        for (int y = 0; y < height; y++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                if(grid[j,y] !=null)
+                {
+                    grid[j, y - 1] = grid[j, y];
+                    grid[j, y] = null;
+                    grid[j, y - 1].transform.position -= new Vector3(0, 1, 0);
+                }
+            }
+
+        }
+    }
+
+    bool ValidMove()
     {
 
         foreach (Transform children in transform)
@@ -74,6 +146,12 @@ public class Block : MonoBehaviour
                 return false;
                 
             }
+
+            if (grid[roundedX,roundedY] !=null)
+            {
+                return false;
+            }
+
         }
 
         return true;
